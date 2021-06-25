ca4df5dd9123d22fee79c25cd5bd09d9338eb9cb
whutfaker <1312976124@qq.com>
2021-06-25 15:39:28 +0800

    feat: add ignore files

--- myqq-server/app/schedule/ping_mysql.js
+++ myqq-server/app/schedule/ping_mysql.js
@@ -1,20 +1,22 @@
+"use strict";
+
 const Subscription = require('egg').Subscription;
 
 class PingMySQL extends Subscription {
-    // 通过 schedule 属性来设置定时任务的执行间隔等配置
-    static get schedule() {
-        return {
-            interval: '1h', // 1 h间隔
-            type: 'all', // 指定所有的 worker 都需要执行
-        };
-    }
+  // 通过 schedule 属性来设置定时任务的执行间隔等配置
+  static get schedule() {
+    return {
+      interval: '1h', // 1 h间隔
+      type: 'all', // 指定所有的 worker 都需要执行
+    };
+  }
 
-    // subscribe 是真正定时任务执行时被运行的函数
-    async subscribe() {
-        //保活
-        let resPing = await this.ctx.app.mysql.get('user', { id: 1 });
-        console.log(new Date().toLocaleTimeString() + "   " + resPing);
-    }
+  // subscribe 是真正定时任务执行时被运行的函数
+  async subscribe() {
+    // 保活
+    const resPing = await this.ctx.app.mysql.get('user', { id: 1 });
+    console.log(new Date().toLocaleTimeString() + '   ' + resPing);
+  }
 }
 
 module.exports = PingMySQL;
